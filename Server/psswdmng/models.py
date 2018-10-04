94a4fe0bdffc959b5797cebe62428cb35779e36a
V-Mor <vvovv266@gmail.com>
2018-10-04 23:06:41 +0700

    starting server development

---
+++ Server/psswdmng/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Resource(models.Model):
+	resource_id = models.AutoField(primary_key = True, unique = True, null = False)
+	URL = models.CharField(max_length = 150, unique = True, null = False)
+		
+	def __str__(self):
+		return self.URL
+
+class Login(models.Model):
+	login_id = models.AutoField(primary_key = True, unique = True, null = False)
+	Login = models.CharField(max_length = 30, unique = True, null = False)
+		
+	def __str__(self):
+		return self.Login
+		
+class Pair(models.Model):
+	pair_id = models.AutoField(primary_key = True, unique = True, null = False)
+	login_id = models.ForeignKey('Login', on_delete = models.CASCADE)
+	
+	def __str__(self):
+		return self.pair_id
+		
+class Main_record(models.Model):
+	pair_id = models.ForeignKey('Pair', on_delete = models.CASCADE, primary_key = True)
+	Password = models.CharField(max_length = 50, null = False)
+	Change_date = models.DateTimeField(auto_now = True, null = False)
