12d8ceefdbc1adf9f8cf6c1e280ca9f3be3b7b25
AgentGosdepa <kot60068@gmail.com>
2018-12-13 02:29:43 +0700

    fix bags

--- Server/psswdmng/views.py
+++ Server/psswdmng/views.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 В данном файле описан набор функций-обработчиков http-запросов к серверу, а таке вспомогательных функций, необходимых для корректной работы сервера.
 Все описанные аргументы функций-обработчиков http-запросов заключены в единственном, передаваемом каждой каждой функции, аргументе request, представляющем
 собой словарь, где ключами являются названия аргументов.
@@ -40,22 +40,22 @@ def index(request):
 # \returns			<b>Password</b> – запрошенный пароль пользователя, если запрос прошёл успешно
 # \returns			<b>Error message</b> – сообщение о непредвиденной ошибке с указаниями возможных действий пользователя по её устранению
 def get_password(request):
-        try:
-			if authentificate(request.GET.getlist('auth_seq'), username = request.GET['username']) != 0:
-				return HttpResponse("Authentification error!")
-			else:
-				user_obj = User.objects.get(User_name = request.GET['username'])
-				user_obj.Authentication_sequence = None
-				user_obj.save()
-			user_obj = User.objects.get(User_name = request.POST['username'])
-			if(user_obj.Session_started == False)
-				return HttpResponse("Your session has already ended")
-			url = request.GET['url']
-			login = request.GET['login']
-			result = Main_record.objects.get(pair_id = Pair.objects.get(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url)), user_id = User.objects.get(User_name = request.GET['username']))
-			return HttpResponse(result.Password)
-		except:
-			return HttpResponse("GET method is required! Send \'auth_seq\', \'username\', \'url\' and \'login\'")
+	try:
+		if authentificate(request.GET.getlist('auth_seq'), username = request.GET['username']) != 0:
+			return HttpResponse("Authentification error!")
+		else:
+			user_obj = User.objects.get(User_name = request.GET['username'])
+			user_obj.Authentication_sequence = None
+			user_obj.save()
+		user_obj = User.objects.get(User_name = request.POST['username'])
+		if(user_obj.Session_started == False):
+			return HttpResponse("Your session has already ended")
+		url = request.GET['url']
+		login = request.GET['login']
+		result = Main_record.objects.get(pair_id = Pair.objects.get(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url)), user_id = User.objects.get(User_name = request.GET['username']))
+		return HttpResponse(result.Password)
+	except:
+		return HttpResponse("GET method is required! Send \'auth_seq\', \'username\', \'url\' and \'login\'")
 
 	
 ## Обработчик http-запроса на добавление нового пользователя
@@ -75,30 +75,30 @@ def get_password(request):
 # \note 				Допустимы только имена, под которыми ещё не были зарегистрированы пользователи в системе. Также запрещено имя \c sys, т.к. оно зарезервировано для нужд системы.
 @csrf_exempt	
 def add_user(request):
-		try:
-			if authentificate(request.POST.getlist('auth_seq'), username = request.POST['user']) != 0:
-				return HttpResponse('Authentification error!')
-			elif(request.POST['user'] != 'sys'):
-				return HttpResponse('Authentification error!')
-			else:
-				user_obj = User.objects.get(User_name = request.POST['user'])
-				user_obj.Authentication_sequence = None
-				user_obj.save()
-			user_obj = User.objects.get(User_name = request.POST['username'])
-			if(user_obj.Session_started == False)
-				return HttpResponse("Your session has already ended")
-			open_key_array = request.POST.getlist('openkey')
-			open_key = b''
-			for ch in open_key_array:
-				open_key += int(ch).to_bytes(1, byteorder = 'little', signed = False)
-			username = request.POST['username']
-			passwordhash = request.POST['passwordhash']
-			User.objects.create(User_name = username, Master_password_hash = passwordhash, Open_key = open_key)
-			return HttpResponse("User was added!")
-		except IntegrityError:
-			return HttpResponse("This user is already exist!")
-		except:
-			return HttpResponse("POST method is required! Send \'username\' and \'passwordhash\'")
+	try:
+		if authentificate(request.POST.getlist('auth_seq'), username = request.POST['user']) != 0:
+			return HttpResponse('Authentification error!')
+		elif(request.POST['user'] != 'sys'):
+			return HttpResponse('Authentification error!')
+		else:
+			user_obj = User.objects.get(User_name = request.POST['user'])
+			user_obj.Authentication_sequence = None
+			user_obj.save()
+		user_obj = User.objects.get(User_name = request.POST['username'])
+		if(user_obj.Session_started == False):
+			return HttpResponse("Your session has already ended")
+		open_key_array = request.POST.getlist('openkey')
+		open_key = b''
+		for ch in open_key_array:
+			open_key += int(ch).to_bytes(1, byteorder = 'little', signed = False)
+		username = request.POST['username']
+		passwordhash = request.POST['passwordhash']
+		User.objects.create(User_name = username, Master_password_hash = passwordhash, Open_key = open_key)
+		return HttpResponse("User was added!")
+	except IntegrityError:
+		return HttpResponse("This user is already exist!")
+	except:
+		return HttpResponse("POST method is required! Send \'username\' and \'passwordhash\'")
 
 				
 ## Обработчик http-запроса на аутентификацию пользователя, сделавшего попытку выполнить вход в систему с программы-клиента
@@ -116,25 +116,25 @@ def add_user(request):
 # \returns				\b 1, аутентификация пользователя провалена
 # \returns				<b>Error message</b> – сообщение о непредвиденной ошибке с указаниями возможных действий пользователя по её устранению
 def authenticate(request):
-		try:
-			if authentificate(request.GET.getlist('auth_seq'), username = request.GET['username']) != 0:
-				return HttpResponse("Authentification error!")
-			else:
-				user_obj = User.objects.get(User_name = request.GET['username'])
-				user_obj.Authentication_sequence = None
-				user_obj.save()
-			user_obj = User.objects.get(User_name = request.POST['username'])
-			if(user_obj.Session_started == False)
-				return HttpResponse("Your session has already ended")
-			username = request.GET['username']
-			passwordhash = request.GET['passwordhash']
-			user = User.objects.get(User_name = username)
-			if str(user.Master_password_hash) == passwordhash:
-				return HttpResponse(1)
-			else:
-				return HttpResponse(0)
-		except:
-			return HttpResponse("GET method is required! Send \'auth_seq\', \'username\' and \'passwordhash\'");
+	try:
+		if authentificate(request.GET.getlist('auth_seq'), username = request.GET['username']) != 0:
+			return HttpResponse("Authentification error!")
+		else:
+			user_obj = User.objects.get(User_name = request.GET['username'])
+			user_obj.Authentication_sequence = None
+			user_obj.save()
+		user_obj = User.objects.get(User_name = request.POST['username'])
+		if(user_obj.Session_started == False):
+			return HttpResponse("Your session has already ended")
+		username = request.GET['username']
+		passwordhash = request.GET['passwordhash']
+		user = User.objects.get(User_name = username)
+		if str(user.Master_password_hash) == passwordhash:
+			return HttpResponse(1)
+		else:
+			return HttpResponse(0)
+	except:
+		return HttpResponse("GET method is required! Send \'auth_seq\', \'username\' and \'passwordhash\'");
 
 
 ## Обработчик http-запроса на добавление основных данных (адрес ресурса, логин и пароль пользователя на ресурсе)
@@ -154,38 +154,38 @@ def authenticate(request):
 # \note 				Если данные, указанные в запросе, не существуют в базе (за исключением имени пользователя), они будут добавлены.				
 @csrf_exempt
 def add_password(request):
-		try:
-			if authentificate(request.POST.getlist('auth_seq'), username = request.POST['username']) != 0:
-				return HttpResponse('Authentification error!')
-			else:
-				user_obj = User.objects.get(User_name = request.POST['username'])
-				user_obj.Authentication_sequence = None
-				user_obj.save()
+	try:
+		if authentificate(request.POST.getlist('auth_seq'), username = request.POST['username']) != 0:
+			return HttpResponse('Authentification error!')
+		else:
 			user_obj = User.objects.get(User_name = request.POST['username'])
-			if(user_obj.Session_started == False)
-				return HttpResponse("Your session has already ended")
-			no_pair = False
-			url = request.POST['url']
-			login = request.POST['login']
-			password = request.POST['password']
-			user = request.POST['username']
-			pair = None
-			if not Login.objects.filter(Login = login).exists():
-				Login.objects.create(Login = login)
-				no_pair = True
-			if not Resource.objects.filter(URL = url).exists():
-				Resource.objects.create(URL = url)
-				no_pair = True
-			if no_pair:
-				pair = Pair.objects.create(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url))
-			elif not Pair.objects.filter(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url)).exists():
-				pair = Pair.objects.create(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url))
-			else:
-				pair = Pair.objects.get(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url))
-			Main_record.objects.create(pair_id = pair, Password = password, user_id = User.objects.get(User_name = user), Change_date = datetime.datetime.now())
-			return HttpResponse(0)
-		except:
-			return HttpResponse("POST method is required! Send \'auth_seq\', \'username\', \'url\', \'login\', \'password\'");
+			user_obj.Authentication_sequence = None
+			user_obj.save()
+		user_obj = User.objects.get(User_name = request.POST['username'])
+		if(user_obj.Session_started == False):
+			return HttpResponse("Your session has already ended")
+		no_pair = False
+		url = request.POST['url']
+		login = request.POST['login']
+		password = request.POST['password']
+		user = request.POST['username']
+		pair = None
+		if not Login.objects.filter(Login = login).exists():
+			Login.objects.create(Login = login)
+			no_pair = True
+		if not Resource.objects.filter(URL = url).exists():
+			Resource.objects.create(URL = url)
+			no_pair = True
+		if no_pair:
+			pair = Pair.objects.create(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url))
+		elif not Pair.objects.filter(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url)).exists():
+			pair = Pair.objects.create(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url))
+		else:
+			pair = Pair.objects.get(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url))
+		Main_record.objects.create(pair_id = pair, Password = password, user_id = User.objects.get(User_name = user), Change_date = datetime.datetime.now())
+		return HttpResponse(0)
+	except:
+		return HttpResponse("POST method is required! Send \'auth_seq\', \'username\', \'url\', \'login\', \'password\'");
         
 
 
@@ -198,18 +198,18 @@ def add_password(request):
 # \returns 			\b enc_auth_seq – зашифрованная последовательность байт, передаваемая пользователю для аутентификации запроса
 # \returns			<b>Error message</b> – сообщение о непредвиденной ошибке с указаниями возможных действий пользователя по её устранению		
 def get_authentication_sequence(request):
-		try:
-			username = request.GET['username']
-			user = User.objects.get(User_name = username)
-			open_key_bytes = bytes(user.Open_key)
-			public_key = load_pem_public_key(open_key_bytes, backend = default_backend())
-			auth_seq = os.urandom(64)
-			user.Authentication_sequence = auth_seq
-			user.save()
-			enc_auth_seq = public_key.encrypt(auth_seq, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-			return HttpResponse(enc_auth_seq, content_type = 'application/octet-stream')
-		except:
-			return HttpResponse("GET method is required! Send \'username\'");
+	try:
+		username = request.GET['username']
+		user = User.objects.get(User_name = username)
+		open_key_bytes = bytes(user.Open_key)
+		public_key = load_pem_public_key(open_key_bytes, backend = default_backend())
+		auth_seq = os.urandom(64)
+		user.Authentication_sequence = auth_seq
+		user.save()
+		enc_auth_seq = public_key.encrypt(auth_seq, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+		return HttpResponse(enc_auth_seq, content_type = 'application/octet-stream')
+	except:
+		return HttpResponse("GET method is required! Send \'username\'");
 
 
 ## Функция, выполняющая аутентификацию запроса	
@@ -219,16 +219,16 @@ def get_authentication_sequence(request):
 # \returns 			\b 1, если запрос не прошёл аутентификацию
 # \returns			\b -1,если возникла непредвиденная ошибка
 def authentificate(auth_seq, username):
-		try:
-			auth_bytes = b''
-			for ch in auth_seq:
-				auth_bytes += int(ch).to_bytes(1, byteorder = 'little', signed = False)
-			if bytes(User.objects.get(User_name = username).Authentication_sequence) != auth_bytes:
-				return 1
-			else:
-				return 0
-		except:
-			return -1
+	try:
+		auth_bytes = b''
+		for ch in auth_seq:
+			auth_bytes += int(ch).to_bytes(1, byteorder = 'little', signed = False)
+		if bytes(User.objects.get(User_name = username).Authentication_sequence) != auth_bytes:
+			return 1
+		else:
+			return 0
+	except:
+		return -1
 
 		
 ## Обработчик http-запроса на замену пароля от ресурса
@@ -247,25 +247,25 @@ def authentificate(auth_seq, username):
 # \returns				<b>Error message</b> – сообщение о непредвиденной ошибке с указаниями возможных действий пользователя по её устранению 
 @csrf_exempt		
 def change_password(request):
-		try:
-			if authentificate(request.POST.getlist('auth_seq'), username = request.POST['username']) != 0:
-				return HttpResponse("Authentification error!")
-			else:
-				user_obj = User.objects.get(User_name = request.POST['username'])
-				user_obj.Authentication_sequence = None
-				user_obj.save()
+	try:
+		if authentificate(request.POST.getlist('auth_seq'), username = request.POST['username']) != 0:
+			return HttpResponse("Authentification error!")
+		else:
 			user_obj = User.objects.get(User_name = request.POST['username'])
-			if(user_obj.Session_started == False)
-				return HttpResponse("Your session has already ended")
-			url = request.POST['url']
-			login = request.POST['login']
-			rec = Main_record.objects.get(pair_id = Pair.objects.get(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url)), user_id = User.objects.get(User_name = request.POST['username']))
-			rec.Password = request.POST['password']
-			rec.Change_date = datetime.datetime.now()
-			rec.save()
-			return HttpResponse(0)
-		except:
-			return HttpResponse("POST method is required! Send \'auth_seq\', \'username\', \'url\', \'login\', \'password\'")
+			user_obj.Authentication_sequence = None
+			user_obj.save()
+		user_obj = User.objects.get(User_name = request.POST['username'])
+		if(user_obj.Session_started == False):
+			return HttpResponse("Your session has already ended")
+		url = request.POST['url']
+		login = request.POST['login']
+		rec = Main_record.objects.get(pair_id = Pair.objects.get(login_id = Login.objects.get(Login = login), resource_id = Resource.objects.get(URL = url)), user_id = User.objects.get(User_name = request.POST['username']))
+		rec.Password = request.POST['password']
+		rec.Change_date = datetime.datetime.now()
+		rec.save()
+		return HttpResponse(0)
+	except:
+		return HttpResponse("POST method is required! Send \'auth_seq\', \'username\', \'url\', \'login\', \'password\'")
 			
 
 ## Обработчик http-запроса на начало новой сессии работы с программой
@@ -295,7 +295,7 @@ def start_session(request):
 		for ch in open_key_array:
 			open_key += int(ch).to_bytes(1, byteorder = 'little', signed = False)
 		user_obj = User.objects.get(User_name = request.POST['username'])
-		if(user_obj.Session_started == True)
+		if(user_obj.Session_started == True):
 			return HttpResponse("Your session has already started")
 		user_obj.Open_key = open_key
 		user_obj.Session_started = True
@@ -327,7 +327,7 @@ def end_session(request):
 			user_obj.save()
 		username = request.POST['username']
 		user_obj = User.objects.get(User_name = request.POST['username'])
-		if(user_obj.Session_started == False)
+		if(user_obj.Session_started == False):
 			return HttpResponse("Your session has already ended")
 		user_obj.Session_started = False
 		user_obj.save()
