5b527c9ab9ffe14f73f079f1b7e081385b0566fb
AgentGosdepa <kot60068@gmail.com>
2018-12-19 22:08:48 +0700

    some tests

--- Server/psswdmng/tests/test_views.py
+++ Server/psswdmng/tests/test_views.py
@@ -5,5 +5,21 @@ from psswdmng.models import *
 #Create your tests here.
 
 class ViewsTest(TestCase):
-    def test_index(self):
+    def test_1(self):
+        pass
+    def test_2(self):
+        pass
+    def test_3(self):
+        pass
+    def test_4(self):
+        pass
+    def test_5(self):
+        pass
+    def test_6(self):
+        pass
+    def test_7(self):
+        pass
+    def test_8(self):
+        pass
+    def test_9(self):
         pass
