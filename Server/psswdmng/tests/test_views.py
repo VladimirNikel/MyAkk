35cb30b5db7e9dcd5b56e97353fe98a4388fb503
AgentGosdepa <kot60068@gmail.com>
2018-12-19 22:52:33 +0700

    tttest

--- Server/psswdmng/tests/test_views.py
+++ Server/psswdmng/tests/test_views.py
@@ -5,21 +5,45 @@ from psswdmng.models import *
 #Create your tests here.
 
 class ViewsTest(TestCase):
-    def test_1(self):
-        pass
-    def test_2(self):
-        pass
-    def test_3(self):
-        pass
-    def test_4(self):
-        pass
-    def test_5(self):
-        pass
-    def test_6(self):
-        pass
-    def test_7(self):
-        pass
-    def test_8(self):
-        pass
-    def test_9(self):
-        pass
+#1-------------------------------------------------------------------------------------------------------
+	def test_index(self):
+		url = reverse('index')
+
+		#test req method GET
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, b"You're in start page")
+		
+		#test req method POST
+		response = self.client.post(url, {})
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, b"You're in start page")
+#2-------------------------------------------------------------------------------------------------------
+	def test_get_password(self):
+		pass
+		#User.objects.create(User_name = 'sys', Master_password_hash = b'123', Open_key = b'123')
+		#User.objects.create(User_name = 'user1', Master_password_hash = b'123', Open_key = b'123')
+#3-------------------------------------------------------------------------------------------------------
+	def test_add_user(self):
+		pass
+#4-------------------------------------------------------------------------------------------------------
+	def test_authenticate(self):
+		pass
+#5-------------------------------------------------------------------------------------------------------
+	def test_add_password(self):
+		pass
+#6-------------------------------------------------------------------------------------------------------
+	def test_get_authentication_sequence(self):
+		pass
+#7-------------------------------------------------------------------------------------------------------
+	def test_authentificate(self):
+		pass
+#8-------------------------------------------------------------------------------------------------------
+	def test_change_password(self):
+		pass
+#9-------------------------------------------------------------------------------------------------------
+	def test_start_session(self):
+		pass
+#10------------------------------------------------------------------------------------------------------
+	def test_end_session(self):
+		pass
